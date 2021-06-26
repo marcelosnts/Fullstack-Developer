1831169a98ad3c49e54432bd4fd3e4bcaaa94546
Marcelo <santosmarcelo1995@live.com>
2021-06-26 14:36:15 -0300

    Add devise routes to api scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'app#index' 
   match '*path', to: 'app#index', via: [:get]
+  
+  scope :api, defaults: { format: :json } do
+    devise_for :users
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
