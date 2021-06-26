3693e1ae26764f60ca767aecb2faf7ae75768b6f
Marcelo <santosmarcelo1995@live.com>
2021-06-26 15:59:55 -0300

    Mv App outside src folder

--- app/javascript/packs/body.js
+++ app/javascript/packs/body.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import App from "@/pages/App";
+import App from "@/App";
 
 document.addEventListener("DOMContentLoaded", () => {
   render(
