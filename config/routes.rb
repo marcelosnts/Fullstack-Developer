c54f410a54e79b231a7dca6e350bcbdfe29db92b
Marcelo <santosmarcelo1995@live.com>
2021-06-27 22:09:28 -0300

    Add resources for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root 'app#index' 
-  match '*path', to: 'app#index', via: [:get]
-  
-  scope :api, defaults: { format: :json } do
-    devise_for :users
-  end
+    root 'app#index' 
+    match '*path', to: 'app#index', via: [:get]
+
+    scope :api, defaults: { format: :json } do
+        devise_for :users
+        resources :users, only: [:update]
+    end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
