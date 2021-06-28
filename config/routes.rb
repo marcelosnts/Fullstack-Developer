79ef59bca7c296380caa3101edc7d0b7bcb13d08
Marcelo <santosmarcelo1995@live.com>
2021-06-28 20:51:19 -0300

    Add users/info index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
     root 'app#index' 
-    match '*path', to: 'app#index', via: [:get]
 
     scope :api, defaults: { format: :json } do
         devise_for :users
-        resources :users, only: [:update]
+        resources :users, only: [:index, :update]
+        resources :info, only: :index
     end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    
+    match '*path', to: 'app#index', via: [:get]
 end
