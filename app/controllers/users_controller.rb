a20ca9e5e0798dbbc24a49e5df7847416ddd0ad9
Marcelo <santosmarcelo1995@live.com>
2021-06-28 20:54:45 -0300

    Add users index method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+    def index
+        users = User.all()
+        render json: users, status: 200
+    end    
+    
     def update
         user = User.find(params[:id])
         user.update(user_params)
