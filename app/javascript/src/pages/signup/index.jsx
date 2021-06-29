6b45f7a541aff3b21f3d2fd4852543a34520fae2
Marcelo <santosmarcelo1995@live.com>
2021-06-29 15:02:52 -0300

    Add form validation / rm semicolon

--- app/javascript/src/pages/signup/index.jsx
+++ app/javascript/src/pages/signup/index.jsx
@@ -1,10 +1,12 @@
-import React, { useCallback, useContext } from 'react'
+import React, { useCallback, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import { Container, TextField, Button } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
+import * as Yup from 'yup'
 
 import Api from '@/services/api'
-import { MainContext } from '@/App'
+import { useToast } from '@/hooks/useToast'
+import getValidationErrors from '@/utils/getValidationErrors'
 
 import './styles.scss'
 
@@ -27,15 +29,23 @@ const useStyles = makeStyles({
 })
 
 export default function Signup(){
-    const classes = useStyles();
-    const history = useHistory();
-    const { updateUser } = useContext(MainContext)
+    const classes = useStyles()
+    const history = useHistory()
+    const [formError, setFormError] = useState({
+        full_name: '',
+        email: '',
+        password: '',
+        password_confirmation: '',
+        avatar_url: '',
+        admin: false,
+    })
+    const { addToast } = useToast()
 
     function handleBack() {
         history.push('/')
     }
 
-    const handleSubmit = useCallback((event) => {
+    const handleSubmit = useCallback(async (event) => {
         event.preventDefault()
         
         const {
@@ -45,22 +55,51 @@ export default function Signup(){
             password_confirmation,
             avatar_image,
             admin
-        } = event.target;
+        } = event.target
 
-        Api.post('/api/users', {
-            user: {
+        try {
+            const schema = Yup.object().shape({
+                full_name: Yup.string().required('The full name field is required'),
+                email: Yup.string().required('The email field is required'),
+                password: Yup.string().required('The password field is required'),
+                password_confirmation: Yup.string().required('You must confirm the password'),
+                avatar_image: Yup.string().required('Insert the user avatar image url'),
+            })
+
+            await schema.validate({
                 full_name: full_name.value,
                 email: email.value,
                 password: password.value,
                 password_confirmation: password_confirmation.value,
                 avatar_image: avatar_image.value,
-                admin: admin.value
+            }, {abortEarly: false})
+
+            Api.post('/api/users', {
+                user: {
+                full_name: full_name.value,
+                email: email.value,
+                password: password.value,
+                password_confirmation: password_confirmation.value,
+                avatar_image: avatar_image.value,
+                admin: admin.value,
+                },
+            }).then(() => {
+                addToast({
+                    type: 'success',
+                    title: 'Welcome!',
+                    description: 'Now feel yourself at home!',
+                })
+
+                history.push('/')
+            })
+        } catch(error) {
+            if (error instanceof Yup.ValidationError) {
+                const serializedErrors = getValidationErrors(error)
+                setFormError(serializedErrors)
+
+                return
             }
-        }).then(response => {            
-            updateUser(response.data)
-            
-            history.push('/')
-        })
+        }
     })
 
     return (
@@ -71,11 +110,36 @@ export default function Signup(){
                     <div className={classes.root}>
                         <div className="mui-text-fields">
                             <input type="hidden" id="admin" value={false}/>
-                            <TextField label="Full Name" id="full_name"/>
-                            <TextField label="Email" id="email"/>
-                            <TextField label="Password" type="password"id="password"/>
-                            <TextField label="Password confirmation" type="password"id="password_confirmation"/>
-                            <TextField label="Avatar url" id="avatar_image"/>
+                            <TextField 
+                                label="Full Name" 
+                                id="full_name"
+                                error={!!formError.full_name}
+                                helperText={formError.full_name}
+                            />
+                            <TextField 
+                                label="Email" 
+                                id="email"
+                                error={!!formError.email}
+                                helperText={formError.email}
+                            />
+                            <TextField 
+                                label="Password" type="password"
+                                id="password"
+                                error={!!formError.password}
+                                helperText={formError.password}
+                            />
+                            <TextField 
+                                label="Password confirmation" type="password"
+                                id="password_confirmation"
+                                error={!!formError.password_confirmation}
+                                helperText={formError.password_confirmation}
+                            />
+                            <TextField 
+                                label="Avatar url" 
+                                id="avatar_image"
+                                error={!!formError.avatar_image}
+                                helperText={formError.avatar_image}
+                            />
                         </div>
                     </div>
                     <div className="button-group">
