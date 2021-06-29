8fa9fb470b483fa408a858c39eaad664e9c0d315
Marcelo <santosmarcelo1995@live.com>
2021-06-29 10:40:47 -0300

    Add users show route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,12 @@ class UsersController < ApplicationController
     def index
         users = User.all()
         render json: users, status: 200
-    end    
+    end
+
+    def show
+        user = User.find(params[:id])
+        render json: user, status: 200
+    end
     
     def update
         user = User.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
     scope :api, defaults: { format: :json } do
         devise_for :users
-        resources :users, only: [:index, :update]
+        resources :users, only: [:index, :show, :update]
         resources :info, only: :index
     end
     
